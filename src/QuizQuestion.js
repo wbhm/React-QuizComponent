bc6e121a99e595767cffba0797b96898ce32197c
wbhm <wbhmoore@gmail.com>
2018-08-06 12:16:04 -0500

    completed module 5

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,9 +3,21 @@ import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { incorrectAnswer: false };
+    }
+
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
+            this.setState(
+                { incorrectAnswer: false }
+            );
             this.props.showNextQuestionHandler();
+        } else {
+            this.setState(
+                { incorrectAnswer: true }
+            );
         }
     }
 
@@ -27,6 +39,9 @@ class QuizQuestion extends Component {
                         }
                     </ul>
                 </section>
+                {
+                    (this.state.incorrectAnswer) ? <p className='error'>Sorry, that's not right</p> : null
+                }
             </main>
         );
     }
